dbf5f9c6ea7a0c5e96ad1a69f213549fe96469b5
gchavez94 <gchavez@d4b.mx>
2025-05-28 13:53:25 -0600

    Committing 1 item from workspace 5bb3a27e-bc84-41d2-bf55-296bb6efc7a4

---
+++ DemosFabric/demo FO/NotebookSparkCarga.Notebook/notebook-content.py
@@ -0,0 +1,46 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "51517173-c1c3-47d2-ba41-c7085ce821d4",
+# META       "default_lakehouse_name": "lake_fo",
+# META       "default_lakehouse_workspace_id": "5bb3a27e-bc84-41d2-bf55-296bb6efc7a4",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "51517173-c1c3-47d2-ba41-c7085ce821d4"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.sql("SELECT * FROM lake_fo.custinvoicetable LIMIT 1000")
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
